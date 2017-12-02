446fece3d22e7bfcf917c080c0335ff536c17352
bethtechow <bethtechow@yahoo.com>
2017-12-02 12:32:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@
 var customerName = `bob`
 console.log(customerName)
 function upperCaseCustomerName(){
-  customerName.toUpperCase()
+  var customerName.toUpperCase()
   console.log(customerName.toUpperCase())
 }
