2c0813b4dedffc57376d5b08abd578b76b191121
bethtechow <bethtechow@yahoo.com>
2017-12-02 12:40:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 // Write your solution in this file!
 var customerName = `bob`
 console.log(customerName)
-function upperCaseCustomerName(){
-  customerName.toUpperCase()
+function upperCaseCustomerName(capsname){
+  let capsname = customerName.toUpperCase()
   console.log(customerName.toUpperCase())
 }
 function setBestCustomer(){
   var bestCustomer = `not bob`
   console.log(bestCustomer)
+}
+function overwriteBestCustomer(){
+  
 }
